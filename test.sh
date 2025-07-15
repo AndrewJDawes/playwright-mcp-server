2ed15b6bccec5021e20895a5a44264114227776b
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-07-15 07:04:59 -0400

    Add -y flag to npx

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # docker run -i --rm -p 8931:8931 -p 5902:5900 playwright-mcp-server:test --vision
+docker build -t playwright-mcp-server:test .
 docker run -i --rm -p 8931:8931 -p 5902:5900 playwright-mcp-server:test
