66ed3c16f8fd7195821734f5ab6cb4cb051af024
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-07-18 06:55:47 -0400

    Add test script

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
 # docker run -i --rm -p 8931:8931 -p 5902:5900 playwright-mcp-server:test --vision
 docker build -t playwright-mcp-server:test .
-docker run -i --rm -p 8931:8931 -p 5902:5900 playwright-mcp-server:test
+docker network create docker_local_dev_playwright_network || true
+docker run -i --rm --name playwright -p 1162:8931 -p 1163:5900 --network docker_local_dev_playwright_network playwright-mcp-server:test
